0446cdb3a91cdc761b8e206d4350fcd03449ec33
José Eduardo Flores Cortez <fc24002@ues.edu.sv>
2026-07-02 09:46:58 -0600

    Fix: actualizar metadata a v1.0.1 y robustecer pausa final en script de arranque, ahora se podra abrir en cualquier terminal

--- Conversor.sh
+++ Conversor.sh
@@ -6,8 +6,22 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # EJECUCION DEL PROGRAMA 
 if [ -f "$DIR/build-linux/TestConversor" ]; then
     # EN ENTORNO DE DESARROLLO 
-    xfce4-terminal --title="Conversor GP9 - Estructura de Datos" --working-directory="$DIR/build-linux" -e "./TestConversor"
+    cd "$DIR/build-linux"
+    ./TestConversor
 else
-    # INSTALADO EN EL SISTEMA EL .deb
-    xfce4-terminal --title="Conversor GP9 - Infija a Postfija" -e "TestConversor"
+    # INSTALADO EN EL SISTEMA MEDIANTE EL .deb
+    # A EATAR EN /usr/bin, SE INVOCA Y ABRE DIRECTAMENTE 
+    TestConversor
 fi
+
+# PAUSA UNIVERSAL EVITA QYE SE CIERRE CUALQUIER TERMINAL QUE EL USUARIO ESTE USANDO EN UN MILISEGUNDO
+echo ""
+echo "------------------------------------------------"
+echo -n "Proceso finalizado. Presione [Enter] para salir..."
+
+# BUCLE PARA QUE SOLO ACEPTE EN ENTER DEL USUARIO Y NO OTROS CARACTERES DONDE SI DETECTA QUE EL CARACTER NO ES UN SALTO DE LINEA ENTER LO IGNORARA Y SEGUIRA ESPERANDO EL ENTER
+while read -r -s -N 1 char; do
+    if [[ "$char" == $'\n' || "$char" == "" ]]; then
+        break
+    fi
+done
